78693f3ab106227495d4093ebcd4a1dd30180099
Robin Karlsson <robin.karlsson@csc.fi>
2021-07-26 16:10:28 +0300

    Increase start timeout

--- template/after.sh
+++ template/after.sh
@@ -2,7 +2,7 @@
 echo "Waiting for Visual studio code server to open port ${port}..."
 echo "TIMING - Starting wait at: $(date)"
 
-if wait_until_port_used "${host}:${port}" 60; then
+if wait_until_port_used "${host}:${port}" 120; then
   echo "Discovered Visual studio code server listening on port ${port}!"
   echo "TIMING - Wait ended at: $(date)"
 else
