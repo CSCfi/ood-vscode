4eae4ab39662b6120da777d7bb61f10be6a7e373
Robin Karlsson <robin.karlsson@csc.fi>
2021-08-13 10:34:12 +0300

    Add form validation

--- ood_install.sh
+++ ood_install.sh
@@ -1,4 +1,5 @@
 mkdir -p "/appl/opt/ood/$1/soft/vscode"      
 cp install.sh "/appl/opt/ood/$1/soft/vscode/"
 cd "/appl/opt/ood/$1/soft/vscode"            
-bash install.sh                    
+bash install.sh
+ln -fns ../../../deps/util/forms/form_validated.js form.js
