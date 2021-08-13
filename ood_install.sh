986c86a37f49049db15341f67770ab70024fa4f2
Robin Karlsson <robin.karlsson@csc.fi>
2021-08-13 10:38:24 +0300

    Fix form.js link

--- ood_install.sh
+++ ood_install.sh
@@ -1,5 +1,5 @@
+ln -fns ../../../deps/util/forms/form_validated.js form.js
 mkdir -p "/appl/opt/ood/$1/soft/vscode"      
 cp install.sh "/appl/opt/ood/$1/soft/vscode/"
 cd "/appl/opt/ood/$1/soft/vscode"            
 bash install.sh
-ln -fns ../../../deps/util/forms/form_validated.js form.js
