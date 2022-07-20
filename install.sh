4fa8ccedc047b6ccad4288d49c86ba182a54811a
Robin Karlsson <robin.karlsson@csc.fi>
2022-07-20 09:11:07 +0300

    Use code-server 4.5.1

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ set +x
 
 rm -rf bin extensions lib  temp_user_data  usr
 
-CODE_VERSION="4.4.0"
+CODE_VERSION="4.5.1"
 curl -#fL -o code-server-${CODE_VERSION}-amd64.rpm -C - https://github.com/coder/code-server/releases/download/v${CODE_VERSION}/code-server-${CODE_VERSION}-amd64.rpm
 rpm2cpio code-server-${CODE_VERSION}-amd64.rpm | cpio -idmv  --no-absolute-filenames
 
@@ -16,8 +16,4 @@ echo '#!/usr/bin/env sh' > bin/code-server
 echo 'DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"' >> bin/code-server
 echo 'exec $DIR/../lib/code-server/bin/code-server "$@"' >> bin/code-server
 rm code-server-${CODE_VERSION}-amd64.rpm
-
-# Make VSCode accept any capitalization on upgrade header (required by Apache mod_proxy_wstunnel)
-file_to_patch="lib/code-server/lib/vscode/out/vs/server/node/server.main.js"
-sed -i 's#headers\.upgrade!=="websocket"#headers.upgrade.toLowerCase()!=="websocket"#g' "$file_to_patch"
 
