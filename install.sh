f614b8008cd417e3fe27e82e26fd8ca9d9ee4850
Henrik Nortamo <hnortamo@csc.fi>
2023-04-06 11:38:49 +0000

    Bump all versions and add julia

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
-
-# This should be run where code-server is intended to be installed, not in the OOD app root
 set -e
 set +x
 
 rm -rf bin extensions lib  temp_user_data  usr
+mkdir temp_user_data
 
-CODE_VERSION="4.6.1"
+CODE_VERSION="4.11.0"
 curl -#fL -o code-server-${CODE_VERSION}-amd64.rpm -C - https://github.com/coder/code-server/releases/download/v${CODE_VERSION}/code-server-${CODE_VERSION}-amd64.rpm
 rpm2cpio code-server-${CODE_VERSION}-amd64.rpm | cpio -idmv  --no-absolute-filenames
 
@@ -17,25 +15,26 @@ echo 'DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"' >
 echo 'exec $DIR/../lib/code-server/bin/code-server "$@"' >> bin/code-server
 rm code-server-${CODE_VERSION}-amd64.rpm
 
+
+JULIA_EXT_VER=1.38.2
+PYTHON_EXT_VER=2023.4.1
+JUPTER_EXT_VER=2023.3.100
+
 EXT_DIR="$PWD/lib/code-server/lib/vscode/extensions"
+./bin/code-server --extensions-dir="$EXT_DIR" --user-data-dir="$PWD/temp_user_data" --verbose --install-extension ms-toolsai.jupyter@$JUPYTER_EXT_VER
+./bin/code-server --extensions-dir="$EXT_DIR" --user-data-dir="$PWD/temp_user_data" --verbose --install-extension ms-python.python@$PYTHON_EXT_VER
+./bin/code-server --extensions-dir="$EXT_DIR" --user-data-dir="$PWD/temp_user_data" --verbose --install-extension julialang.language-julia@$JULIA_EXT_VER
 
-# Fetch Jupyter and Python extensions from the store
-./bin/code-server --extensions-dir="$EXT_DIR" --user-data-dir="$PWD/temp_user_data" --verbose --install-extension ms-toolsai.jupyter
-./bin/code-server --extensions-dir="$EXT_DIR" --user-data-dir="$PWD/temp_user_data" --verbose --install-extension ms-python.python
-
-CPPTOOLS_VERSION="1.12.4"
+CPPTOOLS_VERSION="1.14.5"
 curl -#fL -o cpptools-linux.vsix -C - https://github.com/microsoft/vscode-cpptools/releases/download/v${CPPTOOLS_VERSION}/cpptools-linux.vsix
 ./bin/code-server --extensions-dir="$EXT_DIR" --user-data-dir="$PWD/temp_user_data" --verbose --install-extension cpptools-linux.vsix
-rm cpptools-linux.vsix
+
 
 rm -r temp_user_data
+rm -f cpptools-linux.vsix
 
-# cpptools assumes write mounted FS for first launch
-# deps should be installed already, just need to chmod and add to package.json activationEvents
-# see https://github.com/microsoft/vscode-cpptools/blob/ef52c25aa6b12f2aa810388ba45177b75468443b/Extension/src/main.ts#L206
 chmod +x "$EXT_DIR/ms-vscode.cpptools-${CPPTOOLS_VERSION}/debugAdapters/bin/OpenDebugAD7"
 chmod +x "$EXT_DIR/ms-vscode.cpptools-${CPPTOOLS_VERSION}/bin/cpptools"
 chmod +x "$EXT_DIR/ms-vscode.cpptools-${CPPTOOLS_VERSION}/bin/cpptools-srv"
 chmod +x "$EXT_DIR/ms-vscode.cpptools-${CPPTOOLS_VERSION}/bin/cpptools-wordexp"
 chmod +x "$EXT_DIR/ms-vscode.cpptools-${CPPTOOLS_VERSION}/LLVM/bin/clang-format"
-chmod +x "$EXT_DIR/ms-vscode.cpptools-${CPPTOOLS_VERSION}/LLVM/bin/clang-tidy"
