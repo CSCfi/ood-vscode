d917d9e39dd2bf5c863360bf30e1c101d0ed37d6
Robin Karlsson <robin.karlsson@csc.fi>
2024-09-23 15:29:14 +0300

    Update code-server to 4.92.2 (code 1.92.2)

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ set +x
 
 rm -rf bin extensions lib  temp_user_data  usr
 
-CODE_VERSION="4.89.1"
+CODE_VERSION="4.92.2"
 curl -#fL -o code-server-${CODE_VERSION}-amd64.rpm -C - https://github.com/coder/code-server/releases/download/v${CODE_VERSION}/code-server-${CODE_VERSION}-amd64.rpm
 rpm2cpio code-server-${CODE_VERSION}-amd64.rpm | cpio -idmv  --no-absolute-filenames
 mv usr/* .
